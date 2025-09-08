791972df493a2aba95183d1493daf2177a4aa808
Fran Enríquez <fran.enriquez.dev@gmail.com>
2025-09-08 22:18:10 +0200

    FIX: improve snapshot loading and error handling in TldrawClient; fix save sheets

--- app/(pages)/(protected)/tldraw/TldrawClient.tsx
+++ app/(pages)/(protected)/tldraw/TldrawClient.tsx
@@ -7,9 +7,9 @@ declare global {
     editor?: Editor;
   }
 }
-import { Tldraw, DefaultSpinner } from 'tldraw'
+import { Tldraw, DefaultSpinner, createTLStore } from 'tldraw'
 import type { Editor } from 'tldraw'
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { useAuth } from '../../_hooks/useAuth'
 import useTldraw from './_hooks/useTldraw'
 
@@ -21,10 +21,12 @@ export default function TldrawClient() {
 
   function handleMount(editor: Editor) {
     if (!user) throw new Error("User is required");
-
     const remote = data.snapshot
     if (remote) {
-      editor.loadSnapshot(remote)
+      window.setTimeout(() => {
+        editor.loadSnapshot(remote)
+      }, 500)
+      console.log("Loaded remote snapshot", remote)
     }
 
     let last = ''
@@ -63,6 +65,7 @@ export default function TldrawClient() {
     return () => cleanerEditor();
   }, []);
   
+  console.log({error, loading, snapshot: data?.snapshot})
   return (
     <div className='h-screen w-full'>
         {
@@ -70,7 +73,7 @@ export default function TldrawClient() {
             ? <p className="text-sm text-red-600 mt-2 text-center">{error.message}</p>
             : loading 
               ? <DefaultSpinner /> 
-              : <Tldraw  onMount={handleMount}  persistenceKey="demo-vidext" />
+              : <Tldraw  onMount={handleMount} />
         }
     </div>
   );
